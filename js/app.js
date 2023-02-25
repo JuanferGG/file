efef25b35b65afbf5d5cc6078658864dbbe7685c
JuanferGG <juanfe15777@gmail.com>
2023-02-25 14:18:53 -0500

    version estable 0.5v

--- js/app.js
+++ js/app.js
@@ -15,10 +15,18 @@ function darkMode(){
     document.querySelector('BODY').classList.add('darkBody')
     document.getElementById('darkMode').classList.add('moonHiden')
     document.getElementById('moon').classList.add('sunLogo')
+    
+    //!DarkTitles-P
+    document.getElementsByTagName('h2')[0].classList.add('darkH2')
+    document.getElementsByTagName('p')[0].classList.add('darkP')
 }
 
 function sunMode(){
     document.querySelector('BODY').classList.remove('darkBody')
     document.getElementById('darkMode').classList.remove('moonHiden')
     document.getElementById('moon').classList.remove('sunLogo')
+
+    //!DarkTitles-P
+    document.getElementsByTagName('h2')[0].classList.remove('darkH2')
+    document.getElementsByTagName('p')[0].classList.remove('darkP')
 }
