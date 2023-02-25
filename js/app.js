1f95a4a9d3a01fd64033a37775864383277dd785
JuanferGG <juanfe15777@gmail.com>
2023-02-25 12:35:58 -0500

    version 0.2

--- js/app.js
+++ js/app.js
@@ -1,5 +1,18 @@
 
 
 function navBars(){
-    document.getElementById
+    document.getElementById('barsNav').classList.add('visibleNav')
+}
+function closeNavBars(){
+    document.getElementById('barsNav').classList.remove('visibleNav')
+    document.getElementById('barsNav').classList.remove('closeNav')
+}
+
+
+
+//! DarkMode
+function darkMode(){
+    document.querySelector('BODY').classList.add('darkBody')
+    document.getElementById('moon').classList.add('moonHiden')
+    document.getElementById('darkMode').classList.add('sunVisible')
 }
